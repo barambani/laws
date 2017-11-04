ddcf82348394662fc94896e993b487a887a46e62
barambani <filippo.mariotti@gmail.com>
2017-11-04 19:19:03 +0000

    Added Cartesian

--- src/main/scala/CartesianModule.scala
+++ src/main/scala/CartesianModule.scala
@@ -3,7 +3,17 @@ import scala.language.higherKinds
 object CartesianModule {
 
   trait Cartesian[F[_]] {
-     def product[A, B]: F[A] => F[B] => F[(A, B)]
+    
+    def map2[A, B, C]: F[A] => F[B] => ((A, B) => C) => F[C]
+
+    def map3[A, B, C, D]: F[A] => F[B] => F[C] => ((A, B, C) => D) => F[D] =
+      fa => fb => fc => f => map2(product(fa)(fb))(fc) { (ab, c) => f(ab._1, ab._2, c) }
+    
+    def product[A, B]: F[A] => F[B] => F[(A, B)] =
+      fa => fb => map2(fa)(fb){ (a, b) => (a, b) }
+
+    def product3[A, B, C]: F[A] => F[B] => F[C] => F[(A, B, C)] =
+      fa => fb => fc => map3(fa)(fb)(fc){ (a, b, c) => (a, b, c) }
   }
 
   object Cartesian {
@@ -16,6 +26,21 @@ object CartesianModule {
   }
 
   object CartesianInstances {
-  
+
+    implicit val optionCartesian: Cartesian[Option] = new Cartesian[Option] {
+      def map2[A, B, C]: Option[A] => Option[B] => ((A, B) => C) => Option[C] =
+        fa => fb => f => (fa, fb) match {
+          case (Some(a), Some(b)) => Some(f(a, b))
+          case _ => None
+        }
+    }
+
+    implicit val listCartesian: Cartesian[List] = new Cartesian[List] {
+      def map2[A, B, C]: List[A] => List[B] => ((A, B) => C) => List[C] =
+        fa => fb => f => for {
+          a <- fa
+          b <- fb
+        } yield f(a, b)
+    }
   }
 }
