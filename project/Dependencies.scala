968bc306c7d0ca71c8d3edce9e08f9ab01a35311
Scala steward <scala-steward@timepit.eu>
2018-09-19 01:35:58 +0200

    Update scalacheck to 1.14.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
 
   val externalDependencies = Seq(
     "com.chuusai"     %% "shapeless"    % "2.3.3",
-    "org.scalacheck"  %% "scalacheck"   % "1.13.5"  % "test"
+    "org.scalacheck"  %% "scalacheck"   % "1.14.0"  % "test"
   )
 }
