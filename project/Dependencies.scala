b3656d7e7ab0f159ffb73280e48a78ac76a1e52c
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2019-09-26 12:16:06 +0200

    Update scalacheck to 1.14.2 (#20)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val externalDependencies = Seq(
     "com.chuusai"     %% "shapeless"    % "2.3.3",
     "com.github.ghik" %% "silencer-lib" % "1.4.2",
-    "org.scalacheck"  %% "scalacheck"   % "1.14.1"  % "test",
+    "org.scalacheck"  %% "scalacheck"   % "1.14.2"  % "test",
     compilerPlugin("com.github.ghik" %% "silencer-plugin" % "1.4.2")
   )
 }
